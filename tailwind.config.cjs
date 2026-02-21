89f80e5280423e8212199fc7d8dc0a8165a4edfa
Arlind <arlind@krasniqi-architekt.com>
2026-02-22 00:40:31 +0100

    Change all fonts to Outfit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Outfit', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
